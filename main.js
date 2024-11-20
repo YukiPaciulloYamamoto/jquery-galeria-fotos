a7f2dfdd60c6c0334ee6f19be10940064ec542aa
Yuki <yuki.yamamoto2003@hotmail.com>
2024-11-20 20:08:57 -0300

    FIX: arrumando a ver imagem em tamanho real

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-$(document).ready(function() { //.ready é "quando o documento(HTML) estiver pronto"
-    $('header button').click(function(){ //.click ao realizar o click no botão do header
-        $('form').slideDown(); //Ao clicar no botão "Nova Imagem +" ele faz com que desça a area para adicionar mais fotos
+$(document).ready(function(){
+    $('header button').click(function(){
+        $('form').slideDown();
     })
 
-    $('#botao-cancelar').click(function() { // Ao clicar no botão cancelar ele fecha a aba de adicionar foto
-        $('form').slideUp(); //Ao clicar no botão "Cancelar" ele sobe a aba de adicionar mais fotos
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
     })
 
-    $('form').on('submit', function(e){ // Pegando oq for inserido no input
+    $('form').on('submit', function(e) {
         e.preventDefault();
-        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val(); // o .val é a mesma coisa do .value
-        const novoItem = $('<li style="display: none"></li>');  // Criando constante para colocar novos valores || <li style="display: none"></li> = é nescessario para o efeito fadeIn funcionar (Porque deixa o meu a imagem colocada invisivel)
-        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem); // O .appendTo serve para pegar um elemento e colocar em outro
-        $( // Criando a função para adicionar a barra "Ver imagem em tamanho real"
-            `<div class="overlay-imagem-link">
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
                 <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
                     Ver imagem em tamanho real
                 </a>
             </div>
-        `).appendTo(novoItem); 
+        `).appendTo(novoItem);
         $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn(800);
-        $('#endereco-imagem-nova').val('') // Limpando a caixa de input após apertar no botão
+        $(novoItem).fadeIn(800)
+        $('#endereco-imagem-nova').val('')
     })
 })
